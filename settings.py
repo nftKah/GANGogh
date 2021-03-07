c19dc49b0446cdf0f462476137368effda71bf75
Gudmundur Jonsson <gudjo123@gmail.com>
2021-03-07 18:02:05 +0000

    updated to rodrigo

---
+++ settings.py
@@ -0,0 +1,4 @@
+# Path to training dataset
+ORIGINAL_IMAGES_PATH = '/Users/rodrigobermudezschettino/Developer/github/gangogh/images'
+# Path to store thumbnails of training dataset
+RESIZED_IMAGES_PATH = '/Users/rodrigobermudezschettino/Developer/github/gangogh/data/small/'
--- tflib/plot.py
+++ tflib/plot.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 import matplotlib
-matplotlib.use('Agg')
+
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 import collections
@@ -13,30 +14,34 @@ _since_beginning = collections.defaultdict(lambda: {})
 _since_last_flush = collections.defaultdict(lambda: {})
 
 _iter = [0]
+
+
 def tick():
-	_iter[0] += 1
+    _iter[0] += 1
+
 
 def plot(name, value):
-	_since_last_flush[name][_iter[0]] = value
+    _since_last_flush[name][_iter[0]] = value
+
 
 def flush():
-	prints = []
+    prints = []
 
-	for name, vals in _since_last_flush.items():
-		prints.append("{}\t{}".format(name, np.mean(list(vals.values()))))
-		_since_beginning[name].update(vals)
+    for name, vals in _since_last_flush.items():
+        prints.append("{}\t{}".format(name, np.mean(list(vals.values()))))
+        _since_beginning[name].update(vals)
 
-		x_vals = np.sort(list(_since_beginning[name].keys()))
-		y_vals = [_since_beginning[name][x] for x in x_vals]
+        x_vals = np.sort(list(_since_beginning[name].keys()))
+        y_vals = [_since_beginning[name][x] for x in x_vals]
 
-		plt.clf()
-		plt.plot(x_vals, y_vals)
-		plt.xlabel('iteration')
-		plt.ylabel(name)
-		plt.savefig('generated/'+name.replace(' ', '_')+'.jpg')
+        plt.clf()
+        plt.plot(x_vals, y_vals)
+        plt.xlabel("iteration")
+        plt.ylabel(name)
+        plt.savefig("generated/" + name.replace(" ", "_") + ".jpg")
 
-	print("iter {}\t{}".format(_iter[0], "\t".join(prints)))
-	_since_last_flush.clear()
+    print("iter {}\t{}".format(_iter[0], "\t".join(prints)))
+    _since_last_flush.clear()
 
-	with open('log.pkl', 'wb') as f:
-		pickle.dump(dict(_since_beginning), f, 4)
+    with open("log.pkl", "wb") as f:
+        pickle.dump(dict(_since_beginning), f, 4)
